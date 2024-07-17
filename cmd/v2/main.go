e65b80df77b1d84298fb2c1d34b4b3c9b4654179
Rafael da Costa Nunes <rafael.nunes@appmax.com.br>
2024-07-16 23:15:20 -0300

    added example with wg, pointers and references

---
+++ cmd/v2/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func callDB(wg *sync.WaitGroup) {
+	fmt.Println("Calling DB...")
+	time.Sleep(1 * time.Second)
+	fmt.Println("DB call done.")
+	wg.Done()
+}
+
+func callAPI(wg *sync.WaitGroup) {
+	fmt.Println("Calling API...")
+	time.Sleep(2 * time.Second)
+	fmt.Println("API call done.")
+	wg.Done()
+}
+
+func callExternalService(wg *sync.WaitGroup) {
+	fmt.Println("Calling external service...")
+	time.Sleep(3 * time.Second)
+	fmt.Println("External service call done.")
+	wg.Done()
+}
+
+func main() {
+	fmt.Println("Hello, World!")
+
+	var wg sync.WaitGroup
+	wg.Add(3)
+	go callDB(&wg)
+	go callAPI(&wg)
+	go callExternalService(&wg)
+
+	wg.Wait()
+}
