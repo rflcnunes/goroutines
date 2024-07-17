396ad4964dc32b3d0da51be33600f05c11a01c12
Rafael da Costa Nunes <rafael.nunes@appmax.com.br>
2024-07-17 01:06:01 -0300

    added example with mutex

---
+++ cmd/v3/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func main() {
+	var m sync.Mutex
+
+	i := 0
+	for x := 0; x < 10000; x++ {
+		go func() {
+			m.Lock()
+			i++
+			m.Unlock()
+		}()
+	}
+
+	time.Sleep(time.Second * 5)
+	fmt.Println(i)
+}
